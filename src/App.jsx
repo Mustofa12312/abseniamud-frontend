cf5e02cadfdb1565a334b2086c2c3560a58cab12
Mustofa12312 <cholilelgara001@gmail.com>
2026-09-14 09:43:58 +0700

    saya '

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext"
 import { ToastProvider } from "./contexts/ToastContext"
+import ProtectedRoute from "./components/auth/ProtectedRoute"
 import AuthLayout from "./layouts/AuthLayout"
 import Login from "./pages/auth/Login"
 import LecturerLayout from "./layouts/LecturerLayout"
@@ -39,29 +40,33 @@ function App() {
           </Route>
           
           {/* Lecturer Routes */}
-          <Route path="/lecturer" element={<LecturerLayout />}>
-            <Route index element={<LecturerDashboard />} />
-            <Route path="history" element={<LecturerHistory />} />
-            <Route path="profile" element={<LecturerProfile />} />
-            <Route path="corrections" element={<LecturerCorrection />} />
+          <Route element={<ProtectedRoute allowedRoles={['super_admin', 'admin_akademik', 'dosen']} />}>
+            <Route path="/lecturer" element={<LecturerLayout />}>
+              <Route index element={<LecturerDashboard />} />
+              <Route path="history" element={<LecturerHistory />} />
+              <Route path="profile" element={<LecturerProfile />} />
+              <Route path="corrections" element={<LecturerCorrection />} />
+            </Route>
           </Route>
 
           {/* Admin Routes */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="monitoring" element={<Monitoring />} />
-            <Route path="users" element={<UserMaster />} />
-            <Route path="academic-years" element={<AcademicYearMaster />} />
-            <Route path="locations" element={<LocationMaster />} />
-            <Route path="rooms" element={<RoomMaster />} />
-            <Route path="faculties" element={<FacultyMaster />} />
-            <Route path="courses" element={<CourseMaster />} />
-            <Route path="lecturers" element={<LecturerMaster />} />
-            <Route path="schedules" element={<ScheduleMaster />} />
-            <Route path="reports" element={<ReportMaster />} />
-            <Route path="corrections" element={<CorrectionMaster />} />
-            <Route path="audit-logs" element={<AuditLogMaster />} />
-            <Route path="settings" element={<SystemSettings />} />
+          <Route element={<ProtectedRoute allowedRoles={['super_admin', 'admin_akademik']} />}>
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="monitoring" element={<Monitoring />} />
+              <Route path="users" element={<UserMaster />} />
+              <Route path="academic-years" element={<AcademicYearMaster />} />
+              <Route path="locations" element={<LocationMaster />} />
+              <Route path="rooms" element={<RoomMaster />} />
+              <Route path="faculties" element={<FacultyMaster />} />
+              <Route path="courses" element={<CourseMaster />} />
+              <Route path="lecturers" element={<LecturerMaster />} />
+              <Route path="schedules" element={<ScheduleMaster />} />
+              <Route path="reports" element={<ReportMaster />} />
+              <Route path="corrections" element={<CorrectionMaster />} />
+              <Route path="audit-logs" element={<AuditLogMaster />} />
+              <Route path="settings" element={<SystemSettings />} />
+            </Route>
           </Route>
         </Routes>
       </Router>
